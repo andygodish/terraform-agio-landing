1ade2fb6ba1a41b62f549fca722bac1d1abecfd3
Andy Godish <agodish18@gmail.com>
2024-03-20 16:55:00 -0600

    updated version of linuxwebapp module to 1.0.6

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "appserviceplan" {
 
 module "linuxwebapp" {
   source  = "app.terraform.io/andygio/linuxwebapp/azure"
-  version = "1.0.5"
+  version = "1.0.6"
 
   resource_group_name = module.resourcegroup.rg_name
   asp_plan_id         = module.appserviceplan.asp_id
