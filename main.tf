18f38ef929fd27d838fccde7314c0b593ad09398
Andy Godish <agodish18@gmail.com>
2024-03-20 14:16:49 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+module "resourcegroup" {
+  source  = "app.terraform.io/andygio/resourcegroup/azure"
+  version = "1.0.3"
+
+  app_env    = var.app_env
+  app_prefix = var.app_prefix
+  location   = var.location
+  sub_prefix = var.sub_prefix
+
+  tags = var.tags
+}
+
+module "appserviceplan" {
+  source  = "app.terraform.io/andygio/appserviceplan/azure"
+  version = "1.0.0"
+  
+  resource_group_name = module.resourcegroup.rg_name
+
+  app_env    = var.app_env
+  app_prefix = var.app_prefix
+  location   = var.location
+  sub_prefix = var.sub_prefix
+  asp_sku    = var.asp_sku
+
+  tag = var.tags
+}
+
+module "linuxwebapp" {
+  source  = "app.terraform.io/andygio/linuxwebapp/azure"
+  version = "1.0.1"
+
+  resource_group_name = module.resourcegroup.rg_name
+  asp_plan_id = module.appserviceplan.asp_id
+
+  app_env    = var.app_env
+  app_prefix = var.app_prefix
+  location   = var.location
+  sub_prefix = var.sub_prefix
+
+  tags = var.tags
+}
+
